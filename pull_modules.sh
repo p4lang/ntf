e608e54de283d8862768a70e0673b08f188e12f2
Avinash <avinash.herle@barefootnetworks.com>
2016-07-07 10:33:56 -0700

    updated pull_modules.sh

--- pull_modules.sh
+++ pull_modules.sh
@@ -18,3 +18,9 @@ git clone https://github.com/p4lang/p4-hlir.git
 
 echo "Cloning switch ..."
 git clone https://github.com/p4lang/switch.git
+
+echo "Cloning scapy-vxlan ..."
+sudo apt-get remove python-scapy
+git clone https://github.com/p4lang/scapy-vxlan.git
+cd scapy-vxlan
+sudo python setup.py install
