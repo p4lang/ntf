3b3ae266bc061c81401739e12a22731e6cda61fe
Avinash <avinash.herle@barefootnetworks.com>
2016-07-07 10:52:40 -0700

    Updated pull modules with ref points

--- pull_modules.sh
+++ pull_modules.sh
@@ -6,21 +6,35 @@ cd $WORKSPACE
 
 echo "Cloning ptf ..."
 git clone https://github.com/p4lang/ptf.git
+cd $WORKSPACE/ptf
+git checkout b17f9d2ce75dbee5912127230f9fc909b13d90a5
 
+cd $WORKSPACE
 echo "Cloning p4-bmv2 ..."
 git clone https://github.com/p4lang/behavioral-model.git p4-bmv2
+cd $WORKSPACE/p4-bmv2
+git checkout 8107e1a416de32eedea275d648e75b74b3006539
 
+cd $WORKSPACE
 echo "Cloning p4c-bmv2 ..."
 git clone https://github.com/p4lang/p4c-bm.git p4c-bmv2
+cd $WORKSPACE/p4c-bmv2
+git checkout 2cbf8c40dd1d5badd901427ebae1807616650c40
 
+cd $WORKSPACE
 echo "Cloning p4-hlir"
 git clone https://github.com/p4lang/p4-hlir.git
 
+cd $WORKSPACE
 echo "Cloning switch ..."
-git clone https://github.com/p4lang/switch.git
+git clone https://github.com/barefootnetworks/switch-int-demo.git switch
+cd $WORKSPACE/switch
+git checkout bf8aa22810defc4801d3a1d6119254e9c5ddb937
 
+cd $WORKSPACE
 echo "Cloning scapy-vxlan ..."
 sudo apt-get remove python-scapy
 git clone https://github.com/p4lang/scapy-vxlan.git
 cd scapy-vxlan
+git checkout a63c60e4dd1cf4d4dbc847d3938526a60843363c
 sudo python setup.py install
