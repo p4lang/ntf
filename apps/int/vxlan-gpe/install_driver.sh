c05b323e0c49821eddfa54d5036aeacf9dfe1f97
Shridevi Gurram <shridevi.gurram@gmail.com>
2016-11-18 10:27:33 -0800

    removed old files

--- apps/int/vxlan-gpe/install_driver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-modprobe -r vxlan
-cp vxlan.ko /lib/modules/$(uname -r)/kernel/drivers/net
-modprobe vxlan
--- bmv2/run_build_submodules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-make -f Makefile.bmv2 clean ;
-make -f Makefile.bmv2 driver-switchlink;
